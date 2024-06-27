016248ba7c1c243aa0c28a28332a26a99013e242
root <root@2e0e53316cb9486e897b9dc09adb9412000000.etpbbdongsnexlcfnuibf1j31e.gvax.internal.cloudapp.net>
2024-06-27 13:54:12 +0000

    update

--- scripts/scripts_longBench/eval.sh
+++ scripts/scripts_longBench/eval.sh
@@ -1,10 +1,10 @@
 export CUDA_VISIBLE_DEVICES=$1
 
 method=$2 # Support PyramidKV, SnapKV, H2O, StreamingLLM
-max_capacity_prompts=64 # 128,2048 in paper
-attn_implementation=$3 # Support "flash_attention_2", "sdpa", "eager".
-source_path=$4
-model_path=$5
+max_capacity_prompts=$3 # 128,2048 in paper
+attn_implementation=$4 # Support "flash_attention_2", "sdpa", "eager".
+source_path=$5
+model_path=$6
 save_dir=${source_path}"results_long_bench" # path to result save_dir
 
 python3 run_longbench.py \
